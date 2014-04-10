816aa5aa5874f2fe06a72a154f067106ec8d1779
mnuytemans <michiel.nuytemans@gmail.com>
2014-04-10 11:36:02 +0200

    Session In/Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Brazil::Application.routes.draw do
-  resources :users
+  resources :users, :games
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup', to: 'users#New', via: 'get'
   
@@ -8,6 +9,9 @@ Brazil::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,7 +20,7 @@ Brazil::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
