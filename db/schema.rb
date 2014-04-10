5e4292dd6c8f54e4554496205c434da06879a89c
mnuytemans <michiel.nuytemans@gmail.com>
2014-04-10 16:46:45 +0200

    User changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410085739) do
+ActiveRecord::Schema.define(version: 20140410120545) do
 
   create_table "bets", force: true do |t|
     t.integer  "homescore"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140410085739) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
