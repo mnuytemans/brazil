5ed2717495a4ec82788b5395693421825d439d40
mnuytemans <michiel.nuytemans@gmail.com>
2014-05-01 12:42:57 +0200

    Changed user Profile page

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,17 @@ class Round < ActiveRecord::Base
 	has_one :table
 	has_many :tablebets
 	has_many :betjokers
+	default_scope -> { order ('teams DESC')}
+
+
+
+  def next
+    self.class.unscoped.where("teams <= ? AND id != ?", teams, id).order("teams DESC").first
+  end
+
+  def previous
+    self.class.unscoped.where("teams >= ? AND id != ?", teams, id).order("teams ASC").first
+  end
+
+
 end
