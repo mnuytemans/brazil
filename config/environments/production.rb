b3d35f4e0f7724679a6db429f61864f1d261e824
mnuytemans <michiel.nuytemans@gmail.com>
2014-05-29 17:29:24 +0200

    Bug Hostname Mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Brazil::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => "salty-reaches-5058.herokuapp.com" }
 end
