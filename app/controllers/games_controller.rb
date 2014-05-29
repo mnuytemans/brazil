a0d2a12be5a0b04eec4583329ba868cd7782e42b
mnuytemans <michiel.nuytemans@gmail.com>
2014-05-29 18:07:29 +0200

    Bug fix 2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :admin_user, only: [:index, :create, :new, :update, :edit]
+  before_action :admin_user, only: [:index, :create, :new, :update, :edit, :show]
 
 
   def index
